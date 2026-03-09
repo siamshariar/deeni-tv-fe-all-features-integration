b7171a52ad9a98ad753f1e142338bd1a7bc1eb3f
Siam Al Sahariar <siam.it.int@gmail.com>
2026-03-09 22:21:18 +0600

    Convert app logo jpg to png formate and added favicon-16x16.png file public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,15 +50,15 @@ export const metadata: Metadata = {
   
   icons: {
     icon: [
-      { url: '/App_logo_192.jpg', sizes: '192x192', type: 'image/jpeg' },
+      { url: '/App_logo_192.png', sizes: '192x192', type: 'image/jpeg' },
       { url: '/App_logo_256.jpg', sizes: '256x256', type: 'image/jpeg' },
       { url: '/App_logo_384.jpg', sizes: '384x384', type: 'image/jpeg' },
       { url: '/App_logo_512.jpg', sizes: '512x512', type: 'image/jpeg' },
     ],
     apple: [
-      { url: '/App_logo_192.jpg', sizes: '192x192', type: 'image/jpeg' },
+      { url: '/App_logo_192.png', sizes: '192x192', type: 'image/jpeg' },
     ],
-    shortcut: '/App_logo_192.jpg',
+    shortcut: '/App_logo_192.png',
   },
 
   manifest: '/manifest.json',
